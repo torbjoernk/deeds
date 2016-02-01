40fb55ea1d0122fa55561bf0ab5c68b3d0144f6d
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2016-02-01 07:11:54 +0100

    [spec] for model DeedFormat
    DeedFormat
      has attributes
        material
          as string
          is required
          accepts material 'parchment'
          accepts material 'paper'
          does not accept invalid material key
        width
          as float
          is not required
          must be greater 0.0
        height
          as float
          is not required
          must be greater 0.0
    
    Signed-off-by: Torbjörn Klatt <opensource@torbjoern-klatt.de>

---
+++ spec/factories/deed_formats.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :deed_format_one, class: 'DeedFormat' do
+    material 'parchment'
+    width 1.5
+    height 1.5
+  end
+end
---
+++ spec/models/deed_format_spec.rb
@@ -0,0 +1,78 @@
+require 'rails_helper'
+
+describe DeedFormat, type: :model do
+  context 'has attributes' do
+    context 'material' do
+      it 'as string' do
+        format = create(:deed_format_one)
+        expect(format.material).to be_a String
+      end
+
+      it 'is required' do
+        expect {
+          create(:deed_format_one, material: nil)
+        }.to raise_error ActiveRecord::RecordInvalid
+      end
+
+      DeedFormat::MATERIALS.each do |valid_material|
+        it "accepts material '#{valid_material}'" do
+          expect {
+            create(:deed_format_one, material: valid_material)
+          }.not_to raise_error
+        end
+      end
+
+      it 'does not accept invalid material key' do
+        expect {
+          create(:deed_format_one, material: 'not a material key')
+        }.to raise_error ActiveRecord::RecordInvalid
+      end
+    end
+
+    context 'width' do
+      it 'as float' do
+        format = create(:deed_format_one)
+        expect(format.width).to be_a Float
+
+        expect {
+          create(:deed_format_one, width: 'a string')
+        }.to raise_error ActiveRecord::RecordInvalid
+      end
+
+      it 'is not required' do
+        expect {
+          create(:deed_format_one, width: nil)
+        }.not_to raise_error
+      end
+
+      it 'must be greater 0.0' do
+        expect {
+          create(:deed_format_one, width: 0.0)
+        }.to raise_error ActiveRecord::RecordInvalid
+      end
+    end
+
+    context 'height' do
+      it 'as float' do
+        format = create(:deed_format_one)
+        expect(format.height).to be_a Float
+
+        expect {
+          create(:deed_format_one, width: 'a string')
+        }.to raise_error ActiveRecord::RecordInvalid
+      end
+
+      it 'is not required' do
+        expect {
+          create(:deed_format_one, height: nil)
+        }.not_to raise_error
+      end
+
+      it 'must be greater 0.0' do
+        expect {
+          create(:deed_format_one, height: 0.0)
+        }.to raise_error ActiveRecord::RecordInvalid
+      end
+    end
+  end
+end
