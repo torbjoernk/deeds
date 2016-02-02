da21451c2df1c0710116e1dbb8bf39405a1f4c04
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2016-02-02 19:33:31 +0100

    [model] add NotSelfRelatedValidator
    for PersonRelation and PlaceRelation
    
    Signed-off-by: Torbjörn Klatt <opensource@torbjoern-klatt.de>

---
+++ app/validators/not_self_related_validator.rb
@@ -0,0 +1,17 @@
+class NotSelfRelatedValidator < ActiveModel::Validator
+  def validate(record)
+    raise 'Only for PersonRelation or PlaceRelation' unless record.is_a? PersonRelation or record.is_a? PlaceRelation
+
+    if record.is_a? PersonRelation
+      obj_id = record.person_id
+      obj = Person
+    elsif record.is_a? PlaceRelation
+      obj_id = record.place_id
+      obj = Place
+    end
+
+    if obj_id != nil and record.related_id != nil and obj_id == record.related_id
+      record.errors.add(:self_related, "can not relate a #{obj.name} to its self")
+    end
+  end
+end
