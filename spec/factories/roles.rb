ff965e3131c873a739cceb16df256252aacf2064
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2016-02-02 14:30:50 +0100

    [spec] refactor for model Role
    Role
      has a valid factory
      has attribute
        title as string
        referring as string
        notes as text
      has association with
        many Mentions
        many People through Mentions
        many Places through Mentions
        many Deeds through Mentions
      validates
        presence of
          title
          referring
        value of
          referring
            within ["person", "place", "deed"]
      allows
        absence of
          notes
    
    Signed-off-by: Torbjörn Klatt <opensource@torbjoern-klatt.de>

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -1,4 +1,10 @@
 FactoryGirl.define do
+  factory :role do
+    title Faker::Name.title
+    referring Role::REFERS_TO[Faker::Number.between(0, Role::REFERS_TO.size - 1)]
+    notes Faker::Lorem.paragraphs(2)
+  end
+
   factory :role_one, class: 'Role' do
     title 'A Role for a Person'
     referring 'person'
