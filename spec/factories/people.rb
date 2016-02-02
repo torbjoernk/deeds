5782f898018a080849f813ef07c42a90c6efee82
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2016-02-02 13:14:06 +0100

    [spec] refactor for model Person
    Person
      has a valid factory
      has attribute
        name as string
        gender as string
        notes as text
      has association with
        many People
        many Mentions
        many mentioned Places through Mentions
        many Roles through Mentions
        many Deeds through Mentions
      validates
        presence of
          name
        uniqueness of
          name w.r.t. gender
        value of
          gender
            within ["male", "female"]
      allows
        absence of
          gender
          notes
      methods
        #relatives
          is inverse of related People
    
    Signed-off-by: Torbjörn Klatt <opensource@torbjoern-klatt.de>

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -1,4 +1,10 @@
 FactoryGirl.define do
+  factory :person do
+    name Faker::Name.name
+    gender Person::GENDERS[Faker::Number.between(0, Person::GENDERS.size - 1)]
+    notes Faker::Lorem.paragraphs(2)
+  end
+
   factory :person_one, class: Person do
     name 'Test Person Male'
     gender 'male'
