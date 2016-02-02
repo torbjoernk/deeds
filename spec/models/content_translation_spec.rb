18b2deb23afe1e5fb5691d27f4e388b50e3231d5
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2016-02-02 09:19:35 +0100

    [spec] refactor for model ContentTranslation
    ContentTranslation
      has a valid factory
      has attributes
        translation as string
        language as string
        notes as string
      has association with
        one Content
        one Deed through Content
      validates
        presence of
          translation
          language
        value of
          language
            within ["latin", "old_german", "contemporary_german"]
      allows
        absence of
          notes
    
    Signed-off-by: Torbjörn Klatt <opensource@torbjoern-klatt.de>

--- spec/models/content_translation_spec.rb
+++ spec/models/content_translation_spec.rb
@@ -1,81 +1,84 @@
 require 'rails_helper'
 
 describe ContentTranslation, type: :model do
-  context 'has attributes' do
-    context 'translation' do
-      it 'as string' do
-        translation = create(:content_translation_one)
-        expect(translation.translation).to be_a String
+  before :each do
+    @content = build(:content)
+    @translation = build(:content_translation)
+  end
+
+  it 'has a valid factory' do
+    @translation.save!
+    expect(@translation).to be_valid
+  end
+
+  describe 'has attributes' do
+    specify 'translation as string' do
+      expect(@translation.translation).to be_a String
+    end
+
+    specify 'language as string' do
+      expect(@translation.language).to be_a String
+    end
+
+    specify 'notes as string' do
+      expect(@translation.notes).to be_a String
+    end
+  end
+
+  context 'has association with' do
+    specify 'one Content' do
+      @translation.content = @content
+      @translation.save!
+
+      expect(@translation.content).to eq @content
+      expect(@content.translations).to include @translation
+    end
+
+    specify 'one Deed through Content' do
+      deed = create(:deed)
+      @content.deed = deed
+
+      @translation.content = @content
+      @translation.save!
+
+      expect(@translation.deed).to eq deed
+      expect(deed.translations).to include @translation
+    end
+  end
+
+  describe 'validates' do
+    describe 'presence of' do
+      specify 'translation' do
+        @translation.translation = nil
+        expect(@translation).not_to be_valid
       end
 
-      it 'is required' do
-        expect {
-          create(:content_translation_one, translation: nil)
-        }.to raise_error ActiveRecord::RecordInvalid
+      specify 'language' do
+        @translation.language = nil
+        expect(@translation).not_to be_valid
       end
     end
 
-    context 'language' do
-      it 'as string' do
-        translation = create(:content_translation_one)
-        expect(translation.language).to be_a String
-      end
+    describe 'value of' do
+      describe 'language' do
+        specify "within #{Content::LANGUAGES}" do
+          Content::LANGUAGES.each do |valid_language|
+            @translation.language = valid_language
+            expect(@translation).to be_valid
+          end
 
-      it 'is required' do
-        expect {
-          create(:content_translation_one, language: nil)
-        }.to raise_error ActiveRecord::RecordInvalid
-      end
-
-      Content::LANGUAGES.each do |valid_lang|
-        it "accepts language '#{valid_lang}'" do
-          expect {
-            create(:content_translation_one, language: valid_lang)
-          }.not_to raise_error
+          @translation.language = 'not a language'
+          expect(@translation).not_to be_valid
         end
-      end
-
-      it 'does not accept invalid language' do
-        expect {
-          create(:content_translation_one, language: 'not a language key')
-        }.to raise_error ActiveRecord::RecordInvalid
-      end
-    end
-
-    context 'notes' do
-      it 'as string' do
-        translation = create(:content_translation_one)
-        expect(translation.notes).to be_a String
-      end
-
-      it 'is not required' do
-        expect {
-          create(:content_translation_one, notes: nil)
-        }.not_to raise_error
       end
     end
   end
 
-  context 'has associations with' do
-    context 'content' do
-      it 'belongs to' do
-        translation = create(:content_translation_one)
-        content = Content.find_by(attributes_for(:content_one))
-
-        expect(translation.content).to eq content
-      end
-    end
-
-    context 'deed' do
-      it 'belongs to one through content' do
-        translation = create(:content_translation_one)
-        deed = create(:deed_one)
-
-        deed.content = translation.content
-        deed.save!
-
-        expect(translation.deed).to eq deed
-        expect(deed.translations).to include translation
+  describe 'allows' do
+    describe 'absence of' do
+      specify 'notes' do
+        @translation.notes = nil
+        expect(@translation).to be_valid
       end
     end
   end
