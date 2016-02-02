1e352c10a40224c6655c09f30a2fc6b323a41050
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2016-02-02 11:52:50 +0100

    [spec] refactor for model DeedFormat
    DeedFormat
      has a valid factory
      has attribute
        material as string
        width as float
        height as float
      has association with
        one Deed
      validates
        presence of
          material
        value of
          material
            within ["parchment", "paper"]
          width
            must be greater 0.0
          height
            must be greater 0.0
      allows
        absence of
          width
          height
    
    Signed-off-by: Torbjörn Klatt <opensource@torbjoern-klatt.de>

--- spec/factories/deed_formats.rb
+++ spec/factories/deed_formats.rb
@@ -1,4 +1,10 @@
 FactoryGirl.define do
+  factory :deed_format do
+    material DeedFormat::MATERIALS[Faker::Number.between(0, DeedFormat::MATERIALS.size - 1)]
+    width Faker::Number.positive
+    height Faker::Number.positive
+  end
+
   factory :deed_format_one, class: 'DeedFormat' do
     material 'parchment'
     width 1.5
