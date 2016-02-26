14ac4d2442f8bd2c5ffe73b3556d8da0df2a9077
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2016-02-26 23:54:44 +0100

    [features] complete acceptance tests for Storage
    Signed-off-by: Torbjörn Klatt <opensource@torbjoern-klatt.de>

--- features/step_definitions/storages_steps.rb
+++ features/step_definitions/storages_steps.rb
@@ -2,8 +2,7 @@ Given /^a Storage exists$/ do
   @storage = create :storage
 end
 
-
-When /^the User visits '\/storages'$/ do
+Given /^the User visits '\/storages'$/ do
   visit storages_path
 end
 
@@ -28,9 +27,10 @@ Then /^the User should see a table with storages$/ do
 end
 
 
-And(/^the User clicks on the 'Show' button of the first Storage$/) do
-  find('table#storages > tbody > tr:first-child > td:last-child').
-      click_on "btn-storage-show-details-#{@storage.id}"
+When /^the User clicks on the "([^"]*)" button of the first Storage$/ do |arg|
+  within "#storage-row-#{@storage.id}" do
+    click_on "btn-storage-#{arg.downcase}-#{@storage.id}"
+  end
 end
 
 Then /^the User should see the Storage's details$/ do
@@ -38,3 +38,61 @@ Then /^the User should see the Storage's details$/ do
   expect(find('#storage-modal')).to have_text @storage.notes
 end
 
+
+When /^changes the Storage's title$/ do
+  @new_title = Faker::Name.title
+  fill_in 'Title', with: @new_title
+end
+
+When /^clicks on "([^"]*)"$/ do |arg|
+  click_on arg
+end
+
+Then /^the Storage's title should be changed to the new value$/ do
+  within "#storage-row-#{@storage.id}" do
+    expect(page).to have_text @new_title
+  end
+end
+
+
+When /^the User clicks on the 'Delete' button of the first Storage and accepts the confirmation$/ do
+  within "#storage-row-#{@storage.id}" do
+    accept_confirm do
+      click_on "btn-storage-delete-#{@storage.id}"
+    end
+  end
+end
+
+Then /^the Storage should get deleted$/ do
+  expect(page).not_to have_selector "#storage-row-#{@storage.id}"
+end
+
+
+When /^the User clicks on the 'Delete' button of the first Storage and dismisses the confirmation$/ do
+  within "#storage-row-#{@storage.id}" do
+    dismiss_confirm do
+      click_on "btn-storage-delete-#{@storage.id}"
+    end
+  end
+end
+
+Then /^the Storage should be kept$/ do
+  expect(page).to have_selector "#storage-row-#{@storage.id}"
+end
+
+
+When /^the User clicks on the "([^"]*)" button$/ do |arg|
+  click_on "btn-#{arg.downcase}-storage"
+end
+
+When /^enters the new Storage's title$/ do
+  @new_storage = build(:storage, title: Faker::Name.title)
+  expect(@new_storage.title).not_to eq @storage.title
+  within "#storage-modal" do
+    fill_in 'Title', with: @new_storage.title
+  end
+end
+
+Then /^a new Storage with given title should be created$/ do
+  expect(page).to have_text @new_storage.title
+end
--- features/storages.feature
+++ features/storages.feature
@@ -2,13 +2,36 @@ Feature: Storages
 
   Background:
     Given a Storage exists
+    And the User visits '/storages'
 
   Scenario: Index table
-    When the User visits '/storages'
     Then the User should see a table with storages
 
   @javascript
+  Scenario: Creating a new Storage
+    When the User clicks on the "New" button
+    And enters the new Storage's title
+    And clicks on "Create Storage"
+    Then a new Storage with given title should be created
+
+  @javascript
   Scenario: Show details of a single Storage
-    When the User visits '/storages'
-    And the User clicks on the 'Show' button of the first Storage
+    And the User clicks on the "Show" button of the first Storage
     Then the User should see the Storage's details
+
+  @javascript
+  Scenario: Edit details of a single Storage
+    When the User clicks on the "Edit" button of the first Storage
+    And changes the Storage's title
+    And clicks on "Update Storage"
+    Then the Storage's title should be changed to the new value
+
+  @javascript
+  Scenario: Delete a single Storage
+    When the User clicks on the 'Delete' button of the first Storage and accepts the confirmation
+    Then the Storage should get deleted
+
+  @javascript
+  Scenario: Abort deletion of a single Storage
+    When the User clicks on the 'Delete' button of the first Storage and dismisses the confirmation
+    Then the Storage should be kept
