85e9f4fe00c448d24ce4ffb5c66c3c93222fde9a
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2016-02-29 07:52:47 +0100

    [view][spec] minor adjustments
    Signed-off-by: Torbjörn Klatt <opensource@torbjoern-klatt.de>

--- spec/views/storages/index.html.erb_spec.rb
+++ spec/views/storages/index.html.erb_spec.rb
@@ -13,4 +13,19 @@ describe 'storages/index.html.erb', type: :view do
     expect(render).to have_selector 'table > thead > tr > th + th + th + th'
     expect(render).not_to have_selector 'table > thead > tr > th + th + th + th + th'
   end
+
+  specify 'provides button to create new Storage' do
+    render
+    expect(render).to have_link 'btn-new-storage', href: new_storage_path
+  end
+
+  describe 'when filtered for associated Archive' do
+    specify 'displays associated Archive' do
+      @archive = create :archive
+      render
+
+      expect(render).to have_text 'associated with Archive'
+      expect(render).to have_text @archive.title
+    end
+  end
 end
