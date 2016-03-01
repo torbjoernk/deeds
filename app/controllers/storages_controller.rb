4fea12098b6b148ab33548a8a8ba2d3ebae264ec
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2016-03-01 17:42:02 +0100

    [view][control] nested form for Storage
    Signed-off-by: Torbjörn Klatt <opensource@torbjoern-klatt.de>

--- app/controllers/storages_controller.rb
+++ app/controllers/storages_controller.rb
@@ -31,14 +31,42 @@ class StoragesController < ApplicationController
 
   def edit
     @storage = Storage.find params[:id]
-    respond_to :js
+    if params.has_key? :sub_action
+      if params[:sub_action].to_sym == :refresh_nested
+        @free_archives = Archive.where('id NOT IN (?)',
+                                       ArchiveStorage.select(:archive_id).where(storage_id: @storage))
+      end
+      respond_to do |format|
+        format.js { render partial: 'storages/form/refresh' }
+      end
+    else
+      respond_to do |format|
+        format.js { render 'storages/edit' }
+      end
+    end
   end
 
   def update
     @storage = Storage.find params[:id]
-    @storage.update!(storage_params)
-    flash[:success] = "Updated storage with ID #{@storage.id}."
-    redirect_to storages_path
+    if params.has_key? :sub_action
+      if params.has_key? :archive_id
+        @archive = Archive.find(params[:archive_id])
+        if params[:sub_action].to_sym == :deassociate
+          @storage.archives.delete(@archive) if @storage.archives.include? @archive
+        elsif params[:sub_action].to_sym == :associate
+          @storage.archives << @archive
+          @storage.save!
+        end
+        respond_to do |format|
+          format.js { redirect_to edit_storage_path(@storage, sub_action: :refresh_nested),
+                                  status: :see_other }
+        end
+      end
+    else
+      @storage.update!(storage_params)
+      flash[:success] = "Updated storage with ID #{@storage.id}."
+      redirect_to storages_path
+    end
   end
 
   def destroy
