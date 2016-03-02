f9b77217c4f8c060b284293ad24b61b09fb85871
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2016-03-02 14:50:35 +0100

    [view][helper] model name plus icon from class
    Signed-off-by: Torbjörn Klatt <opensource@torbjoern-klatt.de>

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,6 +1,77 @@
 require 'rails_helper'
 
 describe ApplicationHelper, type: :helper do
+  describe '#icon_for' do
+    describe 'for classes implementing the IconicModel concern' do
+      class IconicTestClass
+        include IconicModel
+        ICON = 'test'
+      end
+
+      specify 'returns FontAwesome icon tag' do
+        expect(icon_for(IconicTestClass)).to eq '<i class="fa fa-fw fa-test"></i>'
+      end
+    end
+
+    describe 'for classes NOT implementing the IconicModel concern' do
+      class TestClass
+      end
+
+      specify 'returns nothing' do
+        expect(Rails.logger).to receive(:warn).with(/No icon defined for model/)
+        expect(icon_for(TestClass)).to eq ''
+      end
+    end
+  end
+
+  describe '#model_name_with_icon' do
+    describe 'for ActiveRecord::Base models' do
+      describe 'that implement the IconicModel concern' do
+        class IconicTestModel < ActiveRecord::Base
+          include IconicModel
+          ICON = 'test'
+        end
+
+        specify 'returns singular humanized name by default' do
+          expect(model_name_with_icon(IconicTestModel)).
+              to eq '<i class="fa fa-fw fa-test"></i> Iconic test model'
+        end
+
+        specify 'returns pluralized human name with :plural specifier' do
+          expect(model_name_with_icon(IconicTestModel, :plural)).
+              to eq '<i class="fa fa-fw fa-test"></i> Iconic test models'
+        end
+      end
+
+      describe 'that do NOT implement the IconicModel concern' do
+        class TestModel < ActiveRecord::Base
+        end
+
+        specify 'returns singular humanized name by default' do
+          expect(model_name_with_icon(TestModel)).to eq 'Test model'
+        end
+
+        specify 'returns pluralized human name with :plural specifier' do
+          expect(model_name_with_icon(TestModel, :plural)).to eq 'Test models'
+        end
+      end
+
+      specify 'raises error for invalid quantity specifier' do
+        expect {
+          model_name_with_icon IconicTestModel, :not_a_quantity
+        }.to raise_error StandardError
+      end
+    end
+
+    describe 'for other classes' do
+      specify 'raises an error' do
+        expect {
+          model_name_from_record_or_class TestClass
+        }.to raise_error StandardError
+      end
+    end
+  end
+
   shared_examples 'link_to_entity_helpers' do |action|
     let(:storage) { create :storage }
     let(:html_link) { send("link_to_#{action}_entity", storage) }
