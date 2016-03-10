d06bd10992ce08c1ecbb2fee17059d30ea3a58ef
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2016-03-10 08:33:14 +0100

    [spec] fix specs w.t.r new index pages
    Signed-off-by: Torbjörn Klatt <opensource@torbjoern-klatt.de>

--- spec/views/contents/index.html.erb_spec.rb
+++ spec/views/contents/index.html.erb_spec.rb
@@ -5,13 +5,7 @@ describe 'contents/index.html.erb', type: :view do
     render
     expect(render).to have_selector 'h1'
     expect(render).to have_selector 'h1 > span.btn-group > a.btn'
-    expect(render).to have_table 'contents'
-  end
-
-  specify 'the table has four columns' do
-    render
-    expect(render).to have_selector 'table > thead > tr > th + th + th + th'
-    expect(render).not_to have_selector 'table > thead > tr > th + th + th + th + th'
+    expect(render).to have_selector 'div#contents-table'
   end
 
   specify 'provides button to create new Content' do
