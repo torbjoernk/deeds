a03a9113d898941fe3a79d0f9d85fd0cfc0bf8ae
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2016-03-16 09:59:12 +0100

    [model] add default Gender 'unknown'
    Signed-off-by: Torbjörn Klatt <opensource@torbjoern-klatt.de>

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,7 @@ class Person < ActiveRecord::Base
   include IconicModel
   ICON = 'user'
 
-  GENDERS = %w(male female)
+  GENDERS = %w(male female unknown)
 
   has_many :person_relations
   has_many :related, through: :person_relations
@@ -12,11 +12,17 @@ class Person < ActiveRecord::Base
   has_many :mentioned_places, through: :mentions, class_name: 'Place', source: :place
   has_many :deeds, through: :mentions, class_name: 'Deed', source: :deed
 
+  before_save :default_values
+
+  def default_values
+    self.gender ||= 'unknown'
+  end
+
   def relatives
     Person.joins(:person_relations).where(person_relations: { related_id: self })
   end
 
   validates :name, presence: true
   validates :name, uniqueness: { scope: :gender }
-  validates :gender, inclusion: { in: GENDERS + [nil] }
+  validates :gender, inclusion: { in: GENDERS }
 end
